7981dcb4648c3657284ef85dbf56c2c67ddbd052
GregoryColombe <52176786+GregoryColombe@users.noreply.github.com>
2022-01-06 22:16:32 +0100

    edit inte and add credits

--- components/single/Home/index.vue
+++ components/single/Home/index.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="home">
-    <h1 class="home-title">
-      The Green Door N°1
-    </h1>
-    <p class="home-description">
-      Parter à la recherche de ce monstre en traçant ses traces de pas.
-    </p>
-    <button class="home-btn">
-      Lancer l'expérience
-    </button>
+    <div>
+      <h1 class="home-title">
+        The Green Door N°1
+      </h1>
+      <p class="home-description">
+        Parter à la recherche de ce monstre en traçant ses traces de pas.
+      </p>
+      <button class="home-btn">
+        Lancer l'expérience
+      </button>
+    </div>
+    <div class="home-credits">
+      <ul>
+        <li><a href="https://www.instagram.com/web_gregory_colombe/" target="_blank">Grégory Colombe</a></li>
+        <li><a href="https://www.instagram.com/andreasbaku/" target="_blank">Andreas Demetriou</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 
